dfb01f7eb8b700e6e6c8bad4a84169ec56a40d44
Jun <timeisonourside@gmail.com>
2014-08-04 19:28:26 -0700

    Put images js style and index.html

---
+++ js/description.js
@@ -0,0 +1,35 @@
+/* $.noConflict(); */
+
+$("document").ready(function($) {
+	$('div.tbm').tinyTips('light', '<strong>The Journey of Luffy</strong><br /><p class="desc">This application software displays assembling a toy ship based on a background story. The background story is a starting episode about a boy\'s journey. The boy Luffy hears his girl friend Boa is kidnapped by Teach, who owns his mysterious island. Luffy decides to go to the island and rescues Boa. Franky is a ship maker who helps Luffy can have a ship for his journey to the island. Luffy starts to catch criminals to earn money for the ship.The story develops interactively by a user\'s decisions. A user can pick a criminal to fight and can select a random number to beat the criminal chosen. The materials used in the story such as characters\' names, plastic figures and a ship are products of Japanese Animation One Piece and Lego.</p>');
+
+	$('div.vmfa').tinyTips('light', '<strong>V-MFA</strong><br /><p class="desc">This web based flash application is designed for users who are interested in fine arts. All information comes from Museum of Fine Arts and it can categorize art-works based on artists, classifications, artists\’ nationality, styles, types and years. Once it categorizes, the 3D-gallery displays and exhibits the art-works.<br />If the virtual galleries are more than two, toggle buttons on the top of screen are activated to switch each room. In the virtual gallery, any artistic masterpiece is clickable to go through the picture-flow screen that contains whole classified art-works. The picture-flow screen consists of a stack of the classified pictures. Each artistic picture explains about the information of it. The magnifier icon on the left can help users scrutinize the picture.</p>');
+	
+	$('div.iid1').tinyTips('light','<strong>A Guide for New Immigrants to America</strong><br /><p class="desc">This is a group project with my colleague, Darren Szeto. The goal of this web application is to educate users regarding immigration information to the US with some gaming factors. The application is not just click-through pages. Users can customize their favorite menu with cute icons on the navigation bar and redesign it if they want. The program also visualizes some information about the population growth in the US. Illustrator and Flash are used for designing vector images and Actionscript3.0 are used for programming.</p>');
+	
+	$('div.iid2').tinyTips('light','<strong>Study of Interactive Media in Fine Art</strong><br /><p class="desc">The mission of this project is to know how much users are satisified with interactive media when they learn an artistic picture by using the interactive media. They use serveal diffent interactive media and may enjoy them or not. The final step of the application is to give them some questionnaire to collect users\' satisfactions and then it shows some statistical data to them.</p>');
+	
+	$('div.ip').tinyTips('light','<strong>Basketball Ver.2</strong><br /><p class="desc">This project is my final project for Intermeditate Programming course at NEU. The web program is a simple basketball game using keyboards. The game is the updated version of the previous basketball game. A special function is added. The function is activated while an user succeeds clean-shots and the shots reaches a certain points. All the animated pictures are hand-drown and programmed by Actionscript 3.0.</p>');
+	
+	$('div.wdc').tinyTips('light','<strong>Won Dharma Center</strong><br /><p class="desc">This project is to renew and renovate the existing web pages for Won Dharma Meditation & Retreat Center. The goal of the renovation is to build the responsive web that many modern websites follow. PHP, MySQL, CSS3, jQuery, LESS and HTML5 are used to build the website.</p>');
+	
+	$('div.rpsls').tinyTips('light', '<strong>Rock-Paper-Scissors-Lizard-Spock</strong><br /><p class="desc">Rock-paper-scissors-lizard-Spock is a variant of Rock-paper-scissors that allows five choices. Each choice wins against two other choices, loses against two other choices and ties against itself. Much of RPSLS\'s popularity is that it has been featured in 3 episodes of the TV series "The Big Bang Theory".</p>');
+	
+	$('div.guess_the_number').tinyTips('light','<strong>Guess the number</strong><br /><p class="desc">The computer picks up a secret number in some known range while a player attempts to guess the number. After each guess, the the computer answers either “Higher”, “Lower” or “Correct!” depending on whether the secret number is higher, lower or equal to the guess.</p>');
+
+	$('div.stopwatch').tinyTips('light','<strong>Stopwatch: The Game</strong><br /><p class="desc">It is built as a simple digital stopwatch that keeps track of the time in tenths of a second. This has a simple game factor. Whenever you stop the watch on each point zero second, a score is added each time.</p>');
+	
+	$('div.pong').tinyTips('light','<strong>Pong Game</strong><br /><p class="desc">Pong is one of the first arcade video games (1972). Tens of lines of Python codes are used to build this Pong game. Use \'w\', \'s\', \'up\', \'down\' for controlling left and right paddles.</p>');
+	
+	$('div.catch').tinyTips('light','<strong>Catch Balls</strong><br /><p class="desc">This is a simple ball catching game. Use arrow keys to catch dropping many balls. Gaining scores depends on what colors of balls are captured. </p>');
+	
+	$('div.memory').tinyTips('light','<strong>Memory Game</strong><br /><p class="desc">This is a memory game to track your memory in order to pair the same number and shape of cards. By clicking, cards are flipped and show their face. Until the cards are matched all together, the game goes on. Have a fun!</p>')
+	
+	$('div.blackjack').tinyTips('light', '<strong>Blackjack</strong><br /><p class="desc">Blackjack is a simple, popular card game that is played in many casinos. Cards in Blackjack have the following values: an ace may be valued as either 1 or 11, face cards (kings, queens and jacks) are valued at 10 and the value of the remaining cards corresponds to their number. During a round of Blackjack, the players plays against a dealer with the goal of building a collection of cards whose cards have a total value that is higher than the value of the dealer\'s cards, but not over 21.</p>')
+	
+	$('div.ricerocks').tinyTips('light', '<strong>Rice Rocks</strong><br /><p class="desc">Rice Rocks is a 2D space game that is inspired by the classic arcade game Asteroids (1979). Asteroids is a relatively simple game by today\'s standards, but was still immensely popular during its time. The player controlls a spaceship and destroys asteroids rolling around the space. The spaceship can be thrusted by the up arrow key and launch missile using the spacebar key.</p>')
+	
+});
+
+
+
